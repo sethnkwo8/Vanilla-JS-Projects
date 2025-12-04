680a12d422cbefe278ee8203f395cebbc633c0a7
Seth Nkwo <sethnkwocool@gmail.com>
2025-12-04 09:45:14 +0100

    Added Background Image and completed API calls functionalities

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,3 +1,102 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = '42c3db1b4b267c1079f220ea349644eb'; // API key
 
+    const weatherSearchForm = document.querySelector('#weatherSearchForm');
+    const weatherSearchInput = document.querySelector('#weatherSearchInput');
+
+    // Event listener to search for weather details through form submission
+    weatherSearchForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const data = await weatherRequest(weatherSearchInput.value, apiKey); // Run fetch function
+        localStorage.setItem('lastSearch', weatherSearchInput.value); // Save last search
+        weatherSearchInput.value = '';
+
+        displayResults(data); // Display results
+    });
+
+    // Function to fetch results from API and return results data
+    async function weatherRequest(city, apiKey) {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+            if (response.status === 404) {
+                console.log('Location not found') // Location searched was not found
+                return null;
+            }
+            if (!response.ok) {
+                throw new Error('Error fetching data'); // Error if fault in response
+            }
+            const data = await response.json();
+            return data;
+        }
+        catch (error) {
+            console.error(error); // Display error
+            return null;
+        }
+    }
+
+    const resultsSearchForm = document.querySelector('#resultsSearchForm');
+    const resultsSearchInput = document.querySelector('#resultsSearchInput');
+    const results = document.querySelector('#results');
+    const home = document.querySelector('#home');
+    const cityName = document.querySelector('#cityName');
+    const temperature = document.querySelector('#temperature');
+    const weatherDescription = document.querySelector('#weatherDescription');
+
+    // Function to display weather results
+    function displayResults(data) {
+        document.body.classList.remove('justify-center');
+        home.classList.add('hidden');
+        results.classList.remove('hidden');
+        results.classList.add('flex', 'flex-col', 'space-y-4', 'items-center');
+
+        // City name and country
+        cityName.textContent = `${data.name}, ${data.sys.country}`;
+
+        // Temperature converted to Celsius
+        const celsiusTemp = Math.round(parseInt(data.main.temp) - 273.15);
+
+        // Display Temperature
+        temperature.innerHTML = `<div>
+                                    <img src='https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png'>
+                                </div> 
+                                <div>
+                                    ${celsiusTemp}&deg;C
+                                </div>`;
+        weatherDescription.textContent = `'${data.weather[0].description.toUpperCase()}'`;
+
+        // Info from API
+        const windSpeedMeters = data.wind.speed;
+        const windSpeedKm = parseInt(windSpeedMeters) * 3.6;
+        const pressure = data.main.pressure;
+        const humidity = data.main.humidity;
+        const visibilityMeters = data.visibility;
+        const visibilityKm = parseInt(visibilityMeters) / 1000;
+
+        // Create div for info cards
+        const info = document.createElement('div');
+        info.classList.add('flex', 'flex-row', 'items-center', 'justify-between', 'w-1/2');
+
+        // Add info cards
+        info.innerHTML = `
+        <div class='info-card'>
+            <p class='text-lg font-bold'>Wind</p>
+            <p class='text-2xl'>${windSpeedKm} km/h ENE</p>
+        </div>
+        <div class='info-card'>
+            <p class='text-lg font-bold'>Pressure</p>
+            <p class='text-2xl'>${pressure} mb/hPa</p>
+        </div>
+        <div class='info-card'>
+            <p class='text-lg font-bold'>Humidity</p>
+            <p class='text-2xl'>${humidity}%</p>
+        </div>
+        <div class='info-card'>
+            <p class='text-lg font-bold'>Visibility</p>
+            <p class='text-2xl'>${visibilityKm}km</p>
+        </div>
+        `
+
+        // Append to results
+        results.appendChild(info);
+    }
 })
