39dd8c20c53c561259a86ee4d689e15b9e180549
Seth Nkwo <sethnkwocool@gmail.com>
2025-12-05 08:32:06 +0100

    Movie Search App creation

--- movie-search-app/app.js
+++ movie-search-app/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const APIKey = 'baa8afb9';
+    const APIKey = '92cb96b17b6ea594ade0b3fe45b93e78';
 
     async function fetchData(apikey) {
 
