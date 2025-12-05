1cf7f707d64a71c9dc552a417835cb5008b9f52d
Seth Nkwo <sethnkwocool@gmail.com>
2025-12-05 22:57:31 +0100

    Completed Top Rated Section

--- movie-search-app/app.js
+++ movie-search-app/app.js
@@ -14,10 +14,10 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Display Trrending results
+    // Display Trending results
     const trending = document.querySelector('#trending');
-    const tvShowsBtn = document.querySelector('#trendingTvShowsBtn');
-    const moviesBtn = document.querySelector('#trendingMoviesBtn');
+    const trendingTvShowsBtn = document.querySelector('#trendingTvShowsBtn');
+    const trendingMoviesBtn = document.querySelector('#trendingMoviesBtn');
     loadTrending();
 
     // Function to fetch trending movies and tv shows
@@ -40,7 +40,8 @@ document.addEventListener('DOMContentLoaded', () => {
     function loadTrending() {
         trending.innerHTML = '';
 
-        moviesBtn.addEventListener('click', async function () {
+        // Display trending movies when movies button is clicked
+        trendingMoviesBtn.addEventListener('click', async function () {
             this.focus();
             trending.innerHTML = '';
             const data = await fetchTrending(APIKey, this.dataset.type);
@@ -60,7 +61,8 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         })
 
-        tvShowsBtn.addEventListener('click', async function () {
+        // Display trending tv shows whentv shows button is clicked
+        trendingTvShowsBtn.addEventListener('click', async function () {
             this.focus();
             trending.innerHTML = '';
             const data = await fetchTrending(APIKey, this.dataset.type);
@@ -79,6 +81,78 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         })
 
-        moviesBtn.click();
+        // Trending movies displayed by default
+        trendingMoviesBtn.click();
+    }
+
+    // Display Top rated results
+    const topRated = document.querySelector('#topRated');
+    const topRatedTvShowsBtn = document.querySelector('#topRatedTvShowsBtn');
+    const topRatedMoviesBtn = document.querySelector('#topRatedMoviesBtn');
+    loadTopRated();
+
+    // Function to fetch top rated movies and tv shows
+    async function fetchTopRated(apikey, type) {
+        try {
+            const response = await fetch(`https://api.themoviedb.org/3/${type}/popular?api_key=${apikey}`);
+            if (!response.ok) {
+                throw new Error('Error fetching data'); // Error if fault in response
+            }
+            const data = await response.json();
+            return data;
+        }
+        catch (error) {
+            console.error(error);
+            return null
+        }
+    }
+
+    // Function to add top rated movies and tv shows
+    function loadTopRated() {
+        topRated.innerHTML = '';
+
+        // Display top rated movies when movies button is clicked
+        topRatedMoviesBtn.addEventListener('click', async function () {
+            this.focus();
+            topRated.innerHTML = '';
+            const data = await fetchTopRated(APIKey, this.dataset.type);
+
+            data.results.slice(0, 10).forEach(result => {
+                const movieCard = document.createElement('div');
+                movieCard.innerHTML = `
+                <a class='movie-card' href='#'>
+                    <div>
+                        <img src='https://image.tmdb.org/t/p/w500/${result.poster_path}' alt='${result.title}'>
+                    </div>
+                    <div>
+                        <p><span class='font-bold text-xl'>${result.original_title}</span>  . <span class='text-sm'>${result.release_date}</span></p>
+                    </div>
+                </a>`;
+                topRated.append(movieCard);
+            });
+        })
+
+        // Display top rated tv shows when tv shows button is clicked
+        topRatedTvShowsBtn.addEventListener('click', async function () {
+            this.focus();
+            topRated.innerHTML = '';
+            const data = await fetchTopRated(APIKey, this.dataset.type);
+
+            data.results.slice(0, 10).forEach(result => {
+                const movieCard = document.createElement('div');
+                movieCard.classList.add('movie-card');
+                movieCard.innerHTML = `
+                <div>
+                    <img src='https://image.tmdb.org/t/p/w500/${result.poster_path}' alt='${result.original_name}'>
+                </div>
+                <div>
+                    <p><span class='font-bold text-xl'>${result.original_name}</span>  . <span class='text-sm'>${result.first_air_date}</span></p>
+                </div>`;
+                topRated.append(movieCard);
+            });
+        })
+
+        // Top rated movies displayed by default
+        topRatedMoviesBtn.click();
     }
 })
